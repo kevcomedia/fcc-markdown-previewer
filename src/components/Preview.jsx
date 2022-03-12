fab1737fe28c0df44f086eb535ba4623a3ca2a61
Kev Comedia <kevcomedia@gmail.com>
2022-03-12 16:55:12 +0800

    Display the parsed markdown in Preview

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,11 +1,12 @@
+import { marked } from 'marked'
+
 const Preview = ({ markdown }) => {
   return (
     <section
       className="border-2 border-sky-800 rounded bg-white p-4 overflow-y-auto h-full"
       id="preview"
-    >
-      {markdown}
-    </section>
+      dangerouslySetInnerHTML={{ __html: marked.parse(markdown) }}
+    />
   )
 }
 

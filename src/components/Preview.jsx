dff4ea7deab1a6feb06c9cd4a24bb89e445c487e
Kev Comedia <kevcomedia@gmail.com>
2022-03-12 19:49:15 +0800

    Move marked into own service

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,16 +1,11 @@
-import { marked } from 'marked'
-
-marked.setOptions({
-  gfm: true,
-  breaks: true,
-})
+import markdownService from '../services/markdownService'
 
 const Preview = ({ markdown }) => {
   return (
     <section
       className="border-2 border-sky-800 rounded bg-white p-4 overflow-y-auto prose"
       id="preview"
-      dangerouslySetInnerHTML={{ __html: marked.parse(markdown) }}
+      dangerouslySetInnerHTML={{ __html: markdownService.parse(markdown) }}
     />
   )
 }

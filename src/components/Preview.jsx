269acfe072591d2c9f2e10df7bba9bf519cbf666
Kev Comedia <kevcomedia@gmail.com>
2022-03-12 17:42:43 +0800

    Use prose style in preview

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -3,7 +3,7 @@ import { marked } from 'marked'
 const Preview = ({ markdown }) => {
   return (
     <section
-      className="border-2 border-sky-800 rounded bg-white p-4 overflow-y-auto h-full"
+      className="border-2 border-sky-800 rounded bg-white p-4 overflow-y-auto h-full prose"
       id="preview"
       dangerouslySetInnerHTML={{ __html: marked.parse(markdown) }}
     />
